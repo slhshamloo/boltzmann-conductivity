5dc51445f2365241971e7e78f0b41537b0009ce8
slhshamloo <slhhero@gmail.com>
2025-04-14 14:31:46 +0200

    tiny rename

--- minimal/FEM_boltzmann_minimum.py
+++ minimal/FEM_boltzmann_minimum.py
@@ -173,8 +173,8 @@ def solve_cyclic_tridiagonal_system(A, b):
     u[-1] = A[0, 0] / free_factor
     # For v, only the dot product is calculated, so only the first
     # and the last elements (the only nonzero elements) are kept
-    v1 = free_factor
-    vN = A[2, -1]
+    v_1 = free_factor
+    v_n = A[2, -1]
 
     # Tridiagonal matrix inversions
     # B^{-1}b
@@ -183,8 +183,8 @@ def solve_cyclic_tridiagonal_system(A, b):
     rank_1_solution = sp.linalg.solve_banded((1, 1), A, u)
 
     # Dot products
-    v_dot_banded_solution = v1*banded_solution[0] + vN*banded_solution[-1]
-    v_dot_rank_1_solution = v1*rank_1_solution[0] + vN*rank_1_solution[-1]
+    v_dot_banded_solution = v_1*banded_solution[0] + v_n*banded_solution[-1]
+    v_dot_rank_1_solution = v_1*rank_1_solution[0] + v_n*rank_1_solution[-1]
 
     # A^{-1}b = B^{-1}b - (B^{-1}u) (v^T B^{-1}b) / (1 + v^T B^{-1}u)
     full_solution = (banded_solution - rank_1_solution
