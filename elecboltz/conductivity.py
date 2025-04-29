Fix derivative matrix

elecboltz/conductivity.py
@@ -296,9 +296,8 @@ class Conductivity:
             opt_einsum.contract('ij,ij->i', u, dkhat)
             for u, dkhat in zip(derivative_directions, self._delta_k_hat)]
         # (vhat x B . dkhat) (delta_{i,j+1} - delta_{i+1,j}) / 2
-        derivative_matrix = [
-            dcomp[None, :] * np.array([-0.5, 0, 0.5]).reshape(-1, 1)
-            for dcomp in derivative_component]
+        derivative_matrix = [dcomp[None, :] * np.array([[0.5], [0.0], [-0.5]])
+                             for dcomp in derivative_component]
         self._differential_operator = [
             out_scattering - e/hbar*derivative for out_scattering, derivative
             in zip(self._out_scattering, derivative_matrix)]