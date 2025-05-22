Fix complementary derivative sum

elecboltz/conductivity.py
@@ -284,14 +284,15 @@ class Conductivity:
             triangle_coordinates - np.roll(triangle_coordinates, -2, axis=1)
         i_idx = self.band.kfaces_periodic
         j_idx = np.roll(self.band.kfaces_periodic, -1, axis=1)
+        k_idx = np.roll(self.band.kfaces_periodic, -2, axis=1)
         n = max(self.band.kpoints_periodic.shape[0],
                 self._derivatives.shape[0])
         np.add.at(
             self._derivatives, ((self._bandwidth+i_idx-j_idx) % n, j_idx),
             self._derivative_components)
         np.add.at(
-            self._derivatives, ((self._bandwidth+j_idx-i_idx) % n, i_idx),
-            -self._derivative_components)
+            self._derivatives, ((self._bandwidth+k_idx-j_idx) % n, j_idx),
+            self._derivative_components)
 
     def _calculate_velocity_projections(self):
         self._vhat_projections = np.zeros((len(self.band.kpoints_periodic), 3))