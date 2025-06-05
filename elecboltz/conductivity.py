1e18c12ed0950c1a05965db72b44d9a1678fb6a6
slhshamloo <slhhero@gmail.com>
2025-06-05 15:45:07 +0200

    Fix scattering function discretization

--- elecboltz/conductivity.py
+++ elecboltz/conductivity.py
@@ -337,7 +337,9 @@ class Conductivity:
                 self._calculate_out_scattering_from_kernel()
 
         if isinstance(self.scattering_rate, Callable):
-            scattering = self.scattering_rate(self.band.kpoints)
+            scattering = self.scattering_rate(self.band.kpoints[:, 0],
+                                              self.band.kpoints[:, 1],
+                                              self.band.kpoints[:, 2])
         else:
             scattering = self.scattering_rate
         # separate the optical conductivity case to avoid making
