e52435c056a145cc7089fab3f918eb699eacaf2d
slhshamloo <slhhero@gmail.com>
2025-07-16 14:47:42 +0200

    Update version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'elecBoltz'
 copyright = '2025, Saleh Shamloo Ahmadi'
 author = 'Saleh Shamloo Ahmadi'
-release = '0.2.0'
+release = '0.2.1'
 
 extensions = [
     'sphinx.ext.autodoc',
