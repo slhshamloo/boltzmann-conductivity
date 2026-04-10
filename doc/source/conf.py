28d4d0983ec475dcb02f44240cafb0598257012c
slhshamloo <slhhero@gmail.com>
2026-04-10 14:32:55 +0200

    Increment version number in doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'elecBoltz'
 copyright = '2025, Saleh Shamloo Ahmadi'
 author = 'Saleh Shamloo Ahmadi'
-release = '0.3.9'
+release = '0.3.10'
 
 extensions = [
     'sphinx.ext.autodoc',
